1a708915d36bb73827e0e85298c8839da1c4a7f8
vedangj044 <vedangj044@gmail.com>
2021-10-16 15:24:31 +0530

    Added loggin

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,13 @@ use std::{
 use anyhow::Result;
 use bytes::Bytes;
 use qp2p::{Config, ConnId, Endpoint};
+use simple_logger::SimpleLogger;
 use tokio::{
     select,
     sync::mpsc::{self, Receiver, Sender},
 };
+
+const ACK: &str = "###ack###";
 
 #[derive(Default, Ord, PartialEq, PartialOrd, Eq, Clone, Copy)]
 struct XId(pub [u8; 32]);
@@ -37,19 +40,31 @@ async fn main() -> Result<()> {
         )
         .await?;
 
-    let (tx, mut rx): (Sender<(SocketAddr, Bytes)>, Receiver<(SocketAddr, Bytes)>) =
-        mpsc::channel(32);
+    SimpleLogger::new().with_level(log::LevelFilter::Debug).init()?;
 
-    let (tx1, mut rx1) = mpsc::channel(32);
+    log::info!("Server started at {}", node.public_addr());
+
+    let (incoming_tx, mut incoming_rx): (
+        Sender<(SocketAddr, Bytes)>,
+        Receiver<(SocketAddr, Bytes)>,
+    ) = mpsc::channel(32);
+
+    let (outgoing_tx, mut outgoing_rx) = mpsc::channel(32);
 
     tokio::spawn(async move {
+
+        // Hashmap of connection id with queue name
         let mut connection_map = HashMap::<SocketAddr, String>::new();
 
+        // Main queue to work as a buffer
         let mut main_queue = HashMap::<String, VecDeque<String>>::new();
+
+        // Available consumers on a queue
         let mut available_queue = HashMap::<String, VecDeque<SocketAddr>>::new();
 
-        while let Some((addr, msg)) = rx.recv().await {
+        while let Some((addr, msg)) = incoming_rx.recv().await {
             let message_str = from_utf8(&msg).unwrap().to_string();
+
             if connection_map.contains_key(&addr) == false {
                 connection_map.insert(addr.clone(), message_str.clone());
 
@@ -61,31 +76,42 @@ async fn main() -> Result<()> {
                     available_queue.insert(message_str.clone(), temp);
                 }
             } else {
-                let queue_name = connection_map.get(&addr).unwrap();
-                if message_str.starts_with("###ack###") == true {
-                    let msg = main_queue.get_mut(queue_name).unwrap().pop_front();
-                    if msg.is_some() {
-                        let res = tx1.send((addr, msg.unwrap())).await;
-                        if res.is_err() {
-                            println!("Error occured hihi");
-                        }
-                    } else {
-                        available_queue.get_mut(queue_name).unwrap().push_back(addr);
-                    }
-                } else {
-                    match available_queue.get_mut(queue_name).unwrap().pop_front() {
-                        Some(i) => {
-                            let res = tx1.send((i, message_str.clone())).await;
-                            if res.is_err() {
-                                println!("Error occured hihi");
+                let queue_name = connection_map.get(&addr);
+                match queue_name {
+                    Some(q) => {
+                        if main_queue.contains_key(q) {
+                            if message_str.starts_with(ACK) {
+                                let msg = main_queue.get_mut(q).unwrap().pop_front();
+                                if msg.is_some() {
+                                    let res = outgoing_tx.send((addr, msg.unwrap())).await;
+                                    if res.is_err() {
+                                        log::error!("Error sending message to cosumer.");
+                                    }
+                                } else {
+                                    available_queue.get_mut(q).unwrap().push_back(addr);
+                                }
+                            } else {
+                                match available_queue.get_mut(q).unwrap().pop_front() {
+                                    Some(i) => {
+                                        let res = outgoing_tx.send((i, message_str.clone())).await;
+                                        if res.is_err() {
+                                            println!("Error occured hihi");
+                                        }
+                                    }
+                                    None => {
+                                        main_queue
+                                            .get_mut(q)
+                                            .unwrap()
+                                            .push_back(message_str.clone());
+                                    }
+                                }
                             }
+                        } else {
+                            log::error!("Queue not found: {}", q);
                         }
-                        None => {
-                            main_queue
-                                .get_mut(queue_name)
-                                .unwrap()
-                                .push_back(message_str.clone());
-                        }
+                    }
+                    None => {
+                        log::error!("Connection {} not connected to any queue", addr);
                     }
                 }
             }
@@ -95,10 +121,12 @@ async fn main() -> Result<()> {
     loop {
         select! {
             Some((addr, msg)) = incoming_messages.next() => {
-                tx.send((addr, msg)).await?;
+                incoming_tx.send((addr, msg)).await?;
+                log::debug!("Message received from {}", addr);
             }
-            Some((addr, msg)) = rx1.recv() => {
+            Some((addr, msg)) = outgoing_rx.recv() => {
                 node.connect_to(&addr).await?.send(Bytes::from(msg.clone())).await?;
+                log::debug!("Message sent to {}", addr);
             }
         }
     }
